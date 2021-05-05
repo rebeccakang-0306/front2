036b964f094dfeb01ed4c0e1e61e98ee2d7da1b1
yinankang <yinank@student.unimelb.edu.au>
2021-05-06 01:08:12 +0800

    hhhh

--- src/pages/Receipts.js
+++ src/pages/Receipts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+{/*import React, { useState } from "react";
 import {useReceipt, updateReceipt, deleteReceipt, useSurvey, deleteSurvey} from "../api";
 
 import Button from "../components/Button";
@@ -6,6 +6,7 @@ import AddReceipt from "../components/AddReceipt";
 import AddSurvey from "../pages/PersonalReceipts";
 import '../styles/Users.css';
 import {ReceiptTable} from "../components/ReceiptTable";
+import {UserExtend} from "./Users";
 export default function Receipts() {
     const { loading, surveys, error } = useSurvey();
     if (loading) {
@@ -43,6 +44,23 @@ export function Receipt(receipt) {
     );
 }
 
+
+export function User(user) {
+    const { _id } = user;
+    const [showUpdate, setShowUpdate] = useState(false);
+
+    return (
+        <div className={`user user-${_id}`} key={_id}>
+            <div className="info">
+                <Button className={"btn"} onClick={() => setShowUpdate(!showUpdate)}>
+                    {showUpdate ? "-" : "+"}
+                </Button>
+            </div>
+            <UserExtend {...user} showUpdate={showUpdate} />
+        </div>
+    );
+}
+
 export function ReceiptExtended(props) {
     const { _id, category, showUpdate } = props;
 
@@ -60,8 +78,84 @@ export function ReceiptExtended(props) {
     return (
         <div className={`author-expand ${showUpdate ? "show" : ""}`}>
             <form>
-                {/* TODO - add value and onChange properties to inputs */}
 
+
+            </form>
+
+        </div>
+    );
+}
+
+*/}
+
+import React, { useState } from "react";
+import {useReceipt, updateReceipt, deleteReceipt} from "../api";
+
+import Button from "../components/Button";
+import AddReceipt from "../components/AddReceipt";
+import AddSurvey from "../pages/PersonalReceipts";
+import '../styles/Users.css';
+import {ReceiptTable} from "../components/ReceiptTable";
+export default function Receipts() {
+    const { loading, receipts, error } = useReceipt();
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    if (error) {
+        return <p>Something went wrong: {error.message}</p>;
+    }
+
+    // use this to make sure you are getting the right data
+    console.log(receipts);
+
+    // Display a list of the receipts
+    return (
+        <div>
+            <h1>Survey Result</h1>
+            <AddSurvey/>
+            <ReceiptTable/>
+        </div>
+    );
+}
+
+export function Receipt(receipt) {
+    const { _id } = receipt;
+    const [showUpdate, setShowUpdate] = useState(false);
+
+    return (
+        <div className={`user user-${_id}`} key={_id}>
+            <div className="info">
+                <Button className={"btn"} onClick={() => setShowUpdate(!showUpdate)}>
+                    {showUpdate ? "-" : "+"}
+                </Button>
+            </div>
+            <ReceiptExtended {...receipt} showUpdate={showUpdate} />
+        </div>
+    );
+}
+
+export function ReceiptExtended(props) {
+    const { _id, category, showUpdate } = props;
+
+    const [id_input, setId] = useState(_id);
+    const [category_input, setCategory] = useState(category);
+
+    function onSubmit() {
+        //call upate receipt function
+        updateReceipt({
+            _id: id_input,
+            category: category_input
+        });
+    }
+
+    return (
+        <div className={`author-expand ${showUpdate ? "show" : ""}`}>
+            <form>
+                {/* TODO - add value and onChange properties to inputs */}
+                <input type="text" name="category_input" value = {category_input} onChange={event => { setCategory(event.target.value);}}/>
+                <Button className={"btn-danger"} onClick={() => deleteReceipt(_id)}>
+                    Delete
+                </Button>
             </form>
 
         </div>
