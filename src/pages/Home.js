ed53ba5eda4688169b2220ae6ecdde40fac4fee8
rebeccakang-0306 <64258260+rebeccakang-0306@users.noreply.github.com>
2021-05-28 12:49:55 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -157,11 +157,12 @@ export default class Login extends Component {
                 </div>
 
         );
-    }s
+    }
     handleSelectChange(e){
         let val = e.target.value
         this.setState({
             userType:val
         })
+    }
 }
 
